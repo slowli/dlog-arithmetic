3be9a06b7370e2d709e8d3cc29a3c9980be89191
Alex Ostrovski <ostrovski.alex@gmail.com>
2019-08-16 14:47:57 +0300

    Update doc example

--- src/lib.rs
+++ src/lib.rs
@@ -10,51 +10,46 @@
 //! };
 //! use rand::thread_rng;
 //!
-//! let mut context = Context::new(Ed25519);
-//! context
-//!     .innermost_scope()
-//!     // Add a selected group generator (basepoint) as a variable `G`.
-//!     .insert_var("G", Value::Element(Ed25519.base_element()))
-//!     // Add a function returning a random scalar, as `rand()`.
-//!     .insert_native_fn("rand", fns::Rand::new(thread_rng()))
-//!     .insert_native_fn("sha512", fns::FromSha512);
+//! let mut context = Context::standard(Ed25519, thread_rng());
 //!
 //! // Language snippet. Syntax is essentially a mix of Python and Rust.
 //! let mut code = r#"
 //!     ## Comments start with a hash sign `#`.
-//!     x = 12345; # `x` is a *scalar* (member of the `Z/nZ` group).
+//!     x = 12345; # `x` is a *scalar* (member of the `Z/nZ` field).
 //!     X = [x]G;  # `X` is a group element obtained by adding the basepoint to itself `x` times.
-//!     X ?= x * G; # `x * G` is another way to write multiplication by a scalar;
-//!                 ##`?=` is equality assertion.
+//!     :assert(X == x * G);
+//!     ## `x * G` is another way to write multiplication by a scalar.
+//!
 //!     ## Element arithmetic uses additive notation:
-//!     X + G ?= [x + 1]G;
+//!     :assert(X + G == [x + 1]G);
 //!     ## Function calls require the function name to be preceded by a colon:
-//!     x = :rand(); # `x` is now a random scalar.
+//!     x = :sc_rand(); # `x` is now a random scalar.
 //!
 //!     ## The language supports tuples and destructuring. For example,
 //!     ## here we create a "keypair".
-//!     keypair = :rand() * (1, G);
-//!     (x, K) = keypair; K ?= [x]G;
+//!     keypair = :sc_rand() * (1, G);
+//!     (x, K) = keypair;
+//!     :assert(K == [x]G);
 //!     ## Tuples of the same type can be added or subtracted, resulting in the tuple
 //!     ## of the same type. Tuples can also be multiplied / divided by a scalar.
-//!     keypair / 2 ?= (x/2, [x/2]G);
+//!     :assert(keypair / 2 == (x/2, [x/2]G));
 //!
 //!     ## Like Rust, language supports blocks and associated variable scopes.
 //!     Y = {
-//!         x = :rand(); # shadows the `x` definition above
+//!         x = :sc_rand(); # shadows the `x` definition above
 //!         [x]G
 //!     };
 //!
 //!     ## It is possible to define user functions:
 //!     fn gen() {
-//!         :rand() * (1, G)
+//!         :sc_rand() * (1, G)
 //!     }
 //!     (x, K) = :gen();
 //!
 //!     ## Functions can take arguments with optional type annotations.
 //!     fn ed25519_sign(x, message: bytes) {
 //!         (r, R) = :gen();
-//!         c = :sha512(R, [x]G, message);
+//!         c = :sc_sha512(R, [x]G, message);
 //!         (R, r + c * x)
 //!     }
 //!     :ed25519_sign(x, "Hello, world!")
